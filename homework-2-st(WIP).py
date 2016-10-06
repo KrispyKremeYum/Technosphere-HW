81b1ae1b429e2a0621e8a2f74c9dfb748cc67286
KrispyKremeYum <alenylm@gmail.com>
2016-10-07 02:35:53 +0400

    Add files via upload

---
+++ homework-2-st(WIP).py
@@ -0,0 +1,237 @@
+
+# coding: utf-8
+
+# # Домашнее задание 2. Преобразование данных
+
+# Цель этого задания -- преобразовать имеющиеся атрибуты пользователей в признаки так, чтобы полученная матрица признаков была пригодна для подачи в алгоритм кластеризации. Этап конструирования признаков -- самый важный и обычно самый долгий. К нему возвращаются много раз на протяжении решения задачи анализа данных.
+# 
+# Кроме библиотек, использованных в первом задании, нам понадобятся следующие библиотеки:
+# 1. [scikit-learn](http://scikit-learn.org/stable/) -- библиотека, реализующая множество алгоритмов машинного обучения и сопутствующих алгоритмов
+
+# In[156]:
+
+import pandas as pd
+import numpy as np
+import pylab as pl
+import sklearn.preprocessing as sp
+import csv
+import re
+import dateutil
+import math as mt
+
+np.set_printoptions(linewidth=150, precision=3, suppress=True)
+
+# Plotting config
+get_ipython().magic(u'pylab inline')
+
+
+# In[157]:
+
+ts_parser = lambda date_str: datetime.datetime.strptime(date_str, "%Y-%m") if pd.notnull(date_str) and date_str else None
+df_users = pd.read_csv("hw1_out.csv", sep="\t", encoding="utf-8", quoting=csv.QUOTE_NONNUMERIC, converters={"created_at": ts_parser})
+# Remove rows with users not found
+df_users = df_users[pd.notnull(df_users['name'])]
+df_users["lat"].fillna(value=0, inplace=True)
+df_users["lon"].fillna(value=0, inplace=True)
+df_users["country"].fillna(value=0, inplace=True)
+df_users = df_users.reset_index(drop=True)
+
+
+# Далее необходимо ввести новые признаки. Для каждого пользователя предлагается ввести следующие признаки:
+# - name_words - количество слов в имени
+# - screen_name_length - количество символов в псевдониме
+# - description_length - длина описания
+# - created_year - год создания аккаунта
+# - country_code - код страны
+# - verified - предлагается перевести в тип int
+# 
+# (2 балла)
+
+# In[159]:
+
+def create_new_features(df_users, features):
+    # Introduce new features
+    new_features = ["name_words", "screen_name_length", "description_length", "created_year", "country_code", "verified"]
+    
+    # Add new_features to features
+    # place your code here
+    features += new_features
+    # Calculate new features and place them into data frame
+    # place tour code here
+    df_tmp = pd.DataFrame({'name_words':[len(df_users['name'][i].split()) for i in xrange(len(df_users))],
+                           'screen_name_length':[len(df_users['name'][i]) for i in xrange(len(df_users))],
+                           'description_length':[len(df_users['description'][i]) if df_users['description'][i] is not NaN else 0 
+                                                 for i in xrange(len(df_users))],
+                           'created_year':[df_users['created_at'][i].to_datetime().year for i in xrange(len(df_users))],
+                           'country_code':[mt.log(int("".join([str(ord(let)-32) 
+                                if df_users['country'][i] is not NaN else str(1) for let in str(df_users['country'][i])]))) 
+                                for i in xrange(len(df_users))],
+                           'uid':df_users['uid']
+                      })
+    df_users.verified.replace([True, False], [1, 0], inplace=True)
+    df_users = pd.merge(df_users, df_tmp, on="uid", how="left")
+    
+    return df_users, features
+
+
+# In[160]:
+
+features = ["lat", "lon", "followers_count", "friends_count", "statuses_count", "favourites_count", "listed_count"]
+df_users, features = create_new_features(df_users, features)
+
+x = df_users[pd.notnull(df_users.cls)][features].values
+y = df_users[pd.notnull(df_users.cls)]["cls"].values
+
+
+# Посмотрим, являются ли какие-либо из выбранных признаков сильно скоррелированными. Для этого посчитаем матрицу корреляций и выберем те пары признаков, абсолютное значения коэффициента корреляции между которыми больше 0.2. Необходимо реализовать функцию find_correlated_features, в которой нужно рассчитать коэффициенты корелляции и вывести те, которые больше 0.2. Подсказка: предлагается найти необходимую функцию в библиотеке np и реализовать find_correlated_features с использованием не более 5 строк кода (включая заголовок функции). (2 балла)
+
+# In[253]:
+
+def find_correlated_features(x, features):
+    # replace this code to find really correlated features
+    for i, feature_i in enumerate(features):
+        for j, feature_j in enumerate(features):
+            if np.corrcoef([row[i] for row in x], [row[j] for row in x])[0, 1] > 0.2 and i != j:
+                print "Correlated features: %s + %s -> %.2f" % (feature_i, feature_j, 0.51)
+    
+
+
+# In[254]:
+
+find_correlated_features(x, features)
+
+
+# Выделилось 3 группы признаков:
+# 1. Основанные на географии:  "lat", "lon", "country_code"
+# 2. Основанные на социальной активности:  "verified", "followers_count", "friends_count", "statuses_count", "favourites_count", "listed_count", "created_year"
+# 3. Остальные:  "name_words", "screen_name_length", "description_length"
+# 
+# Построим взаимные распределения пар признаков в каждой из групп, а также гистограмму значений каждого из признаков с учетом целевой переменной.
+Необходимо реализовать функции: plot_two_features_scatter для построения взаимного распределения пары признаков, plot_feature_histogram для построения гистограммы значений, plot_dataset для построения набора графиков по разным парам признаков. (4 балла)
+# In[ ]:
+
+def plot_two_features_scatter(x_i, x_j, y):
+    
+    # Set colors and plot scatter
+    # your code here
+    
+    pass    
+
+    
+def plot_feature_histogram(x_i, y):
+    
+    # Compute positive and negative histograms
+    # your code here
+    
+    # Plot stacked barplots
+    # your code here
+    
+    pass
+
+
+def plot_dataset(x, y, features):
+    # Tune your plot if necessary
+    # your code here
+
+    for i, feature_i in enumerate(features):
+        for j, feature_j in enumerate(features):
+            
+            # Tune your plot if necessary (for example set labels)
+            # your code here
+            
+            # Do actual plotting
+            if i != j:
+                plot_two_features_scatter(x[:, i], x[:, j], y)            
+            else:
+                plot_feature_histogram(x[:, i], y)
+    
+    pl.show()
+
+
+# Построим попарные распределения географических признаков ([подсказка](http://stroykova.github.io/sphera/l2_1.png)).
+
+# In[ ]:
+
+geo_features_new = ["lat", "lon", "country_code"]
+geo_features = [f for f in geo_features_new if f in features]
+
+geo_feature_ind = [i for i, f in enumerate(features) if f in geo_features]
+plot_dataset(x[:, geo_feature_ind], y, geo_features)
+
+
+# Четко видны очертания карты и то, что большинство пользователей происходят из небольшого набора стран. Если принять во внимание конечную цель -- кластеризацию пользователей -- логично предположить, что использование географических признаков для описания пользователя может оказаться не очень полезным. Причина в том, что эти признаки четко пространственно разделены (как минимум, океанами и морями). Поэтому мы рискуем вместо "интересной" кластеризации получить просто кластеры, которые будут представлять разные страны. В дальнейшем мы исключим географические признаки из рассмотрения при кластеризации пользователей.
+# 
+# Далее построим попарные распределения социальных признаков ([подсказка](http://stroykova.github.io/sphera/l2_2.png)).
+
+# In[ ]:
+
+social_features_new = ["verified", "followers_count", "friends_count", "statuses_count", "favourites_count", "listed_count", "created_year"]
+social_features = [f for f in social_features_new if f in features]
+social_feature_ind = [i for i, f in enumerate(features) if f in social_features]
+plot_dataset(x[:, social_feature_ind], y, social_features)
+
+
+# Из графиков видно, что признаки "followers_count", "friends_count", "statuses_count", "favourites_count", "listed_count" сильно смещены в сторону небольших значений. В таком случае удобно сделать логарифмическое преобразрвание этих признаков, то есть применить к их значениям $x_{ij}$ функцию $\log(1 + x_{ij})$. Сделаем это и построим новые распределения ([подсказка](http://stroykova.github.io/sphera/l2_3.png)). Необходимо реализовать функцию log_transform_features, которая выполняет указанное логарифмическое преобразование. (2 балла)
+
+# In[ ]:
+
+def log_transform_features(data, features, transformed_features):
+    # place your code here
+    # transform selected features with log function
+    return data
+
+
+# In[ ]:
+
+transformed_features = ["followers_count", "friends_count", "statuses_count", "favourites_count", "listed_count"]
+x = log_transform_features(x, features, transformed_features)
+
+# Re-plot features
+plot_dataset(x[:, social_feature_ind], y, social_features)
+
+
+# Сразу бросается в глаза, что признак "verified" сильно смещен -- верифицированных пользователей очень мало. Более того, все верифицированные пользователи имеют много фолловеров, поэтому часть информации о верификации дублируется в признаке "followers_count". По этой причине в дальнейшем не будем рассматривать признак "verified".
+# 
+# После того как мы с помощью логрифмического преобразования избавились от сильной скошенности признаков, можно наблюдать некоторые интересные зависимости. Например, пользователи, имеющие много фолловеров, обязательно имеют много статусов. Следовательно, чтобы стать популярным, обязательно нужно много писать. Анализ других зависимостей остается как упражнение.
+# 
+# Наконец построим попарные распределения остальных признаков ([подсказка](http://stroykova.github.io/sphera/l2_4.png)).
+
+# In[ ]:
+
+other_features_new = ["name_words", "screen_name_length", "description_length"]
+other_features = [f for f in other_features_new if f in features]
+other_feature_ind = [i for i, f in enumerate(features) if f in other_features]
+plot_dataset(x[:, other_feature_ind], y, other_features)
+
+
+# Итак после первичной обработки данных мы имеем 9 числовых признаков, каждый из которых распределен в некотором своем интервале. Для того, чтобы ни один признак не получил перевеса при кластеризации, нормализуем данные так, что каждый признак распределен на отрезке $[0, 1]$. 
+
+# In[ ]:
+
+selected_features = ["followers_count", "friends_count", "statuses_count", "favourites_count", 
+                     "listed_count", "created_year", "name_words", "screen_name_length", "description_length"]
+
+x_1 = df_users[selected_features].values
+y = df_users["cat"].values
+
+# x_1 = x[:, selected_features_ind]
+# Replace nan with 0-s
+# Is there a smarter way?
+x_1[np.isnan(x_1)] = 0
+x_min = x_1.min(axis=0)
+x_max = x_1.max(axis=0)
+x_new = (x_1 - x_min) / (x_max - x_min)
+
+
+# Упакуем полученную матрицу в pandas DataFrame и сохраним в файл "hw2_out.csv". В следующем задании мы будем кластеризовать пользователей на оновании этих данных.
+
+# In[ ]:
+
+df_out = pd.DataFrame(data=x_new, index=df_users["uid"], columns=[f for f in selected_features])
+df_out.to_csv("hw2_out.csv", sep="\t")
+
+
+# In[ ]:
+
+
+
